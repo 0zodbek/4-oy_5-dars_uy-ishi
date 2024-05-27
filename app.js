34809a507c0e1fa267f671474c2b5702df9ffba7
0zodbek <ozpdbekov@gmail.com>
2024-05-27 09:10:25 +0500

    masalalar

---
+++ app.js
@@ -0,0 +1,241 @@
+                // 1-masala
+
+// let n = +prompt("n ni kiriting;");
+
+// for( let i = 1 ; i <= n ; i++){
+// console.log(i);
+// };
+
+
+                // 2-masala
+
+// let n = +prompt("n ni kiriting;");
+
+// for( let i = n ; i >= 1 ; i-- ){
+// console.log(i);
+// };
+
+
+                // 3-masala
+
+// let n = +prompt("n ni kiriting;");
+
+// let sum = 0 ;
+
+// for( i = 1 ; i <= n ; i++){
+// sum = sum + i ;
+// }
+
+// console.log(sum);
+
+                // 4-masala
+
+// let n = +prompt(" n ni kiriing;");
+
+// let sum = 0 ;
+
+// for( i = 1 ; i <= n ; i++){
+// if(i % 2 == 0){
+// console.log(i);
+// sum = sum + i ;
+// }
+// };
+
+// console.log(sum);
+
+                // 5-masala
+
+// let n = +prompt(" n ni kiriing;");
+
+// let sum = 0 ;
+
+// for( i = 1 ; i <= n ; i++){
+// if(i % 2 == 1){
+// console.log(i); 
+// sum = sum + i ; 
+// }
+// };
+// console.log(sum);
+
+
+                // 6-masala
+
+// let n = +prompt("n ni kiriting;");
+
+// for(i = 1 ; i <= n ; i++){
+// if(n % i == 0 ){
+// console.log(i);
+// }
+// };
+
+
+                //    7-masala
+
+// let n = +prompt("n ni kiriting;");
+
+// let sum = 0 ;
+
+// let counter = 0 ;
+
+// for( i = 1 ; i <= n ; i++ ){
+// if( n % i == 0 ){
+// counter = counter + 1 ;
+// sum = sum + i ;
+// }
+// }
+// console.log(counter);
+// console.log(sum);
+
+
+// 8-masala
+
+
+
+
+
+
+
+
+
+
+
+
+                // 9-masala
+
+
+
+                // 10-masala
+
+// let a = +prompt("a sononi kiriting;");
+
+// let b = +prompt("b sonini kiriting;");
+
+// for(i = a ; i <= b ; i++ ){
+//     console.log(i);
+// };
+
+
+// 11-masala
+
+// alert("a < b");
+
+// let a = +prompt("a ni kiriting;");
+
+// let b = +prompt("b ni kiriting;");
+
+// for( i = b-- ; i > a ; i-- ){
+// console.log(i);
+// };
+
+
+// 12-masala
+
+// let kg = +prompt("1 kg konfet narxini kiriting;");
+
+// let result = 0;
+
+// for(i = 1 ; i <= 10 ; i++){
+// console.log(kg*i);
+// result = kg*i;
+// }
+
+
+// 13-masala
+
+// let kg = +prompt("1 kg konfet narxini kiriting;");
+
+// let result = 0;
+
+// for(i = 0.1 ; i <= 1 ; i += 0.1){
+// console.log(kg*i);
+// result = kg*i;
+// }
+
+
+// 14-masala
+
+// let kg = +prompt("1 kg konfet narxini kiriting;");
+
+// let result = 0;
+
+// for(i = 1.2 ; i <= 2 ; i += 0.2){
+// console.log(kg*i);
+// result = kg*i;
+// }
+
+
+// 15-masala
+
+// alert("(a < b) a va b sonlarini kiriting;");
+
+// let a = +prompt(" a ni kiriting;");
+
+// let b = +prompt(" b ni kiriting;");
+
+// let sum = 0 ;
+
+// for(i = a ; i <= b ; i++){
+
+// sum +=i ;
+// };
+// console.log(sum);
+
+
+// 16-masala
+
+// alert("(a < b) a va b sonlarini kiriting;");
+
+// let a = +prompt(" a ni kiriting;");
+
+// let b = +prompt(" b ni kiriting;");
+
+// let sum = 1 ;
+
+// for(i = a ; i <= b ; i++){
+
+// sum = sum * i ;
+// };
+// console.log(sum);
+
+
+// 17-masala
+
+// alert("a < b a va b sonlarini kiriting;");
+
+// let a = +prompt("a ni kiriting;");
+
+// let b = +prompt("b ni kiriting;");
+
+// let sum = 0 ;
+
+// for(i = a ; i < b ; i++){
+// sum = sum + i*i ;
+// }
+// console.log(sum);
+
+
+// 18-masala
+
+// let n = +prompt(" n ni kiriting;");
+
+// sum = 0 ;
+
+// for( i = 1 ; i <= n ; i++ ){
+// sum = sum + 1/i ;
+// }
+// console.log(sum);
+
+
+// 19-masala
+
+// alert("n sonini kiriting;");
+
+// let n = +prompt(" n ni kiriting;");
+
+// let sum = 0 ;
+
+// for( i = n ; i <= 2*(n*n) ; i++){
+// sum = sum + i*i ;
+// }
+// console.log(sum);
+
